b89f8745c7f1815ae6f48434267e2d69ed8c4f0e
Laksanai <laksanai555@hotmail.com>
2023-07-30 17:24:37 +0700

    Array Filter

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let scores = [82, 75, 48, 64, 36];
-let passFailScores = scores.map((score) => {
-    if (score >= 50) {
-        return 'passed';
-    };
-    return 'failed';
+let passScores = scores.filter((score) => {
+    return score >= 50;
 });
 
 console.log(scores);
-console.log(passFailScores);
+console.log(passScores);
