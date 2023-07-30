b8afecf30c7bb56d784a89f50e7653ef8307cc13
Laksanai <laksanai555@hotmail.com>
2023-07-30 17:15:27 +0700

    Array Forloop ForEach

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-let fruits = ['Apple', 'Banana', 'Carrot'];
-console.log(fruits);
+let scores = [82, 75, 48, 64, 36];
+let passedCount = 0;
 
-// ขนาดของ array
-console.log(fruits.length);
+// old method
+// for (let i = 0; i < scores.length; i++) {
+//     if (scores[i] >= 50 ) {
+//         passedCount++;
+//     }
+// }
 
-// การเข้าถึง array แต่ละตัว
-// fruits[1] = 'Watermelon';
-// console.log(fruits[1]);
+scores.forEach((score) => {
+    if (score >= 50) {
+        passedCount++;
+    }
+})
 
-// การเข้าเพิ่มข้อมูลตัวทายของ array
-// fruits.push('Watermelon'); //Add data at end position
-// console.log(fruits)
 
-// การลบข้อมูลตัวท้ายของ array
-// fruits.pop(); //Delete last one Object
-// console.log(fruits)
-
-// การแทรกข้อมูลใน array
-// fruits.splice(1, 0, 'Watermelon'); //ถ้าเพิ่มข้อมูลล (ตำแหน่งที่จะเพิ่มข้อมูล, จำนวนข้อมูลที่ต้องการลบ, ค่าที่จะเพิ่มเข้าไป)
-// // fruits.splice(1, 1); //ถ้าลบ (ตำแหน่งที่จะเพิ่มข้อมูล, จำนวนข้อมูลที่ต้องการลบ)
-// console.log(fruits);
-
-// การลบข้อมูลในตำแหน่งที่แทรกใน array
-// fruits.splice(1, 1); //ถ้าลบ (ตำแหน่งที่จะเพิ่มข้อมูล, จำนวนข้อมูลที่ต้องการลบ)
-// console.log(fruits)
+console.log(passedCount);
