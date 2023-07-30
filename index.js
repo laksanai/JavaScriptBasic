05031b56d60af5961e173fd2a94e2f7806ac811a
Laksanai <laksanai555@hotmail.com>
2023-07-30 17:35:59 +0700

    Array find and findIndex

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let scores = [82, 75, 48, 64, 36];
-let sumScores = scores.reduce((sum, score) => {
-    return sum+score;
-});
+
+// let failedScore = scores.find((score) => {
+//     return score < 50;
+// }); 
+
+// console.log(scores);
+// console.log(failedScore);
+
+let failedScore = scores.findIndex((score) => {
+    return score < 50;
+}); 
 
 console.log(scores);
-console.log(sumScores);
+console.log(failedScore);
